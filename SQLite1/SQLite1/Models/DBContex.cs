bcba7ac2631a44363136563cd29f39e80ce5b81c
= <=>
2019-07-25 09:36:30 -0600

    Primer commit

---
+++ SQLite1/SQLite1/Models/DBContex.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SQLite1.Models
+{
+  class DBContex
+  {
+    public SQLiteAsyncConnection Conexion { get; set; }
+
+    public DBContex(string path)
+    {
+      try
+      {
+        this.Conexion = new SQLiteAsyncConnection(path);
+        this.Conexion.CreateTableAsync<Producto>().Wait();
+      }
+      catch (Exception e)
+      {
+
+        Debug.Print("Error" + e.Message);
+      }
+
+    }
+
+
+  }
+}
---
+++ SQLite1/SQLite1/Models/Producto.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite1.Models
+{
+  class Producto
+  {
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public double Precio { get; set; }
+    public string Descripcion { get; set; }
+
+    private DBContex db;
+
+    //ctor vacio
+    public Producto()
+    {
+
+    }
+
+    //ctor con parametro_contex
+    public Producto(DBContex database)
+    {
+      this.db = database;
+    }
+
+    // GET ALL
+    public Task<List<Producto>> Query(string sql)
+    {
+      return this.db.Conexion.QueryAsync<Producto>(sql);
+    }
+
+    // ADD
+    public Task<bool> Agregar(Producto pro)
+    {
+      if (this.db.Conexion.InsertAsync(pro).Result == 1)
+      {
+        return Task.FromResult(true);
+      }
+      else
+      {
+        return Task.FromResult(false);
+      }
+    }
+
+    // DELETE
+    public Task<bool> Eliminar(Producto pro)
+    {
+      if (this.db.Conexion.DeleteAsync(pro).Result == 1)
+      {
+        return Task.FromResult(true);
+      }
+      else
+      {
+        return Task.FromResult(false);
+      }
+    }
+
+    // UPDATE
+    public Task<bool> Actualizar(Producto pro)
+    {
+      if (this.db.Conexion.UpdateAsync(pro).Result == 1)
+      {
+        return Task.FromResult(true);
+      }
+      else
+      {
+        return Task.FromResult(false);
+      }
+  
+    }
+
+
+
+
+  }
+}
